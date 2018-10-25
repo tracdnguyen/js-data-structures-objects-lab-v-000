05ec1e97eb76a4348de0eda6438a37d7eb05cb63
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-25 06:08:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign({}, obj, { [key]: value) });
+  return Object.assign({}, driver, { [key]: value) });
 }
