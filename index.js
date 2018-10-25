20d29d36fe0b48b383a68dbba1736cafe87fa766
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-25 06:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign(driver, { [key]: value})
 }
+
+function deleteFromDriverByKey(driver, key) {
+
+}
